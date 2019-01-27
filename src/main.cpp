37b46324bc4f52c772824a805041b394c03bd1a6
Romualdo Dasig <dasig.rg@a5project.com>
2019-01-27 14:34:10 +0800

    Tested getting an access token.

--- src/main.cpp
+++ src/main.cpp
@@ -157,13 +157,55 @@ void saveMessageToContacts() {
   // SECRET_CREDENTIAL
 
   // Get access token.
-  numdata = inet.httpGET("dev-elexparts-api.pantheonsite.io", 80, "/rest/session/token", msg, 500);
+  numdata = inet.httpGET("dev-elexparts-api.pantheonsite.io", 80, "/rest/session/token", msg, 1000);
 
   // Print the results.
-  Serial.println("\nGET - Number of data received:");
-  Serial.println(numdata);
-  Serial.println("\nGET - Data received:");
-  Serial.println(msg);
+  // Serial.println("\nGET - Number of data received:");
+  // Serial.println(numdata);
+  // Serial.println("\nGET - Data received:");
+  // Serial.println(msg);
+
+  /**
+  HTTP/1.1 200 OK
+  Cache-Control: must-revalidate, no-cache, private
+  Content-Language: en
+  Expires: Sun, 19 Nov 1978 05:00:00 GMT
+  Server: nginx
+  X-Content-Type-Options: nosniff
+  X-Debug-Token: e29f2c
+  X-Debug-Token-Link: /admin/reports/profiler/view/e29f2c
+  X-Frame-Options: SAMEORIGIN
+  X-Generator: Drupal 8 (https://www.drupal.org)
+  X-Pantheon-Styx-Hostname: styx-fe3-a-5794d5fd7-qpvrq
+  X-Styx-Req-Id: styx-24f5a9ba846f9378b2cb5a6c4998e4a1
+  X-Ua-Compatible: IE=edge
+  Accept-Ranges: bytes
+  Accept-Ranges: bytes
+  Via: 1.1 varnish
+  Age: 0
+  Accept-Ranges: bytes
+  Accept-Ranges: bytes
+  Date: Sun, 27 Jan 2019 05:57:57 GMT
+  Via: 1.1 varnish
+  Connection: close
+  X-Served-By: cache-mdw17345-MDW, cache-hkg17926-HKG
+  X-Cache: MISS, MISS
+  X-Cache-Hits: 0, 0
+  X-Timer: S1548568881.732217,VS0,VE268
+  Vary: 
+  RIC: 
+  Content-Type: text/plain;charset=UTF-8
+  Content-Length: 43
+
+  D6t_CjaEE0buRV677WFUajX28DjBqyfH8iw_99zPMZY
+
+  CLOSED
+
+  */
+
+  // We're able to get an http response when requesting for access token.
+  // Now we need to parse it.
+  char access_token[60] = "D6t_CjaEE0buRV677WFUajX28DjBqyfH8iw_99zPMZY";
 
   // HTTP Headers.
   // Authorization Basic SECRET_CREDENTIAL
@@ -183,7 +225,7 @@ void saveMessageToContacts() {
       }
     }
   }
-   */
+  */
 
   // Do a POST request.
   // const char* parameters;
@@ -211,5 +253,5 @@ void loop() {
   // Send an auto-reply message.
   // autoReply();
 
-  delay(5000);
+  delay(30000);
 };
